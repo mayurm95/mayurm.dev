2c70b3e138ea8fe1ac7157de33c0904f1cc5cfb1
Mayur Mahajan <mahajanmayur1995@gmail.com>
2025-06-23 22:06:53 +0530

    3 commit

--- mayurm.dev/src/component/ui/MenuButton.tsx
+++ mayurm.dev/src/component/ui/MenuButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function MenuButton({className}: any) {
+export default function MenuButton({className}: {className:string}) {
     return (
         <button className={`${className}`} type="button" id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" >
             <div className="flex flex-col h-[50px] gap-2 rounded-md w-[3rem] p-2 bg-[red]">
